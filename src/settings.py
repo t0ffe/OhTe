5ad80603a6bb498fef3675da6dbd89a9feb33674
t0f <toffelaakso@gmail.com>
2023-04-04 18:48:23 +0300

    vko3 HT2 addition

--- src/settings.py
+++ src/settings.py
@@ -1,8 +1,24 @@
 import pygame as pg
 
+vec = pg.math.Vector2
+
 TILE_SIZE = 20
 FIELD_SIZE = FIELD_W, FIELD_H = 10, 20
 FIELD_RES = FIELD_W * TILE_SIZE, FIELD_H * TILE_SIZE
 
 FPS = 60
 FIELD_COLOR = (55, 55, 55)
+
+INITIAL_POSITION_OFFSET = vec(FIELD_SIZE) // 2
+
+TETROMINOES = ["I","J","L","O","S","T","Z"]
+
+TETROMINOE_SHAPES = [
+    [(0, 0), (0, 1), (0, -1), (0, -2)],
+    [(0, 0), (-1, 0), (0, -1), (0, -2)],
+    [(0, 0), (1, 0), (0, -1), (0, -2)],
+    [(0, 0), (0, -1), (1, 0), (1, -1)],
+    [(0, 0), (-1, 0), (0, -1), (1, -1)],
+    [(0, 0), (-1, 0), (1, 0), (0, 1)],
+    [(0, 0), (1, 0), (0, -1), (-1, -1)]
+]
---
+++ src/tetromino.py
@@ -0,0 +1,25 @@
+from settings import *
+import random as ra
+
+class Block(pg.sprite.Sprite):
+    def __init__(self, tetromino, position):
+        self.tetromino = tetromino
+        self.position = vec(position) + INITIAL_POSITION_OFFSET
+
+        super().__init__(tetromino.tetris.sprite_group)
+        self.image = pg.Surface([TILE_SIZE, TILE_SIZE])
+        self.image.fill('white')
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = self.position * TILE_SIZE
+
+class Tetromino:
+    def __init__(self, tetris):
+        self.shape_num = ra.randint(0,6)
+        self.tetris = tetris
+        self.shape = TETROMINOES[self.shape_num]
+        print(self.shape)
+        self.blocks = [Block(self, position) for position in TETROMINOE_SHAPES[self.shape_num]]
+
+    def update(self):
+        pass
